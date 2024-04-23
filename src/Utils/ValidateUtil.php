085de63e139e9f9ab10043d83c52b25431a9a526
lyz <yulong.li@meta-sight.com>
2024-04-23 10:57:20 +0800

    ✨feat ValidateUtil

---
+++ src/Utils/ValidateUtil.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lyz\Utils;
+
+/**
+ * Class ValidateUtil
+ * 验证工具类
+ * @package Lyz\Utils
+ */
+class ValidateUtil
+{
+    /**
+     * 是否手机号
+     * 
+     * @param string $phone
+     * @return bool
+     */
+    public static function checkPhone($phone)
+    {
+        if (preg_match("/^1[3456789]{1}\d{9}$/", $phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 是否邮箱
+     * 
+     * @param string $email
+     * @return bool
+     */
+    public static function checkEmail($email)
+    {
+        $pattern = "/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/";
+        if (preg_match($pattern, $email)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
